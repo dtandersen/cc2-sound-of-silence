a5a4ba92764b6d8a81f45ff79520b0b94069952f
David Andersen <git@davidandersen.us>
2022-06-08 14:49:56 -0700

    ambient wind and wave sounds

--- build.bat
+++ build.bat
@@ -1,20 +1,22 @@
 set SOURCE_AUDIO=source\audio
 set CONTENT_AUDIO=content\audio
-set LOW="volume=-20dB"
-set MED="volume=-10dB"
+set LOWLOW="volume=-25dB"
+set LOW="volume=-22dB"
+set MEDLOW="volume=-17dB"
+set MED="volume=-14dB"
 set NORMAL="volume=1"
-set SILENT="volume=0.05"
+set SILENT="volume=0"
 
-REM rmdir /s /q content
+rmdir /s /q content
 mkdir content\audio
 
-ffmpeg -y -i %SOURCE_AUDIO%\cc_sfx_vehicle01_rpm_01_loop.wav -filter:a %SILENT% -acodec libvorbis %CONTENT_AUDIO%\cc_sfx_vehicle01_rpm_01_loop.ogg
-ffmpeg -y -i %SOURCE_AUDIO%\cc_sfx_vehicle01_rpm_02_loop.wav -filter:a %SILENT% -acodec libvorbis %CONTENT_AUDIO%\cc_sfx_vehicle01_rpm_02_loop.ogg
-ffmpeg -y -i %SOURCE_AUDIO%\cc_sfx_vehicle02_rpm_01_loop.wav -filter:a %SILENT% -acodec libvorbis %CONTENT_AUDIO%\cc_sfx_vehicle02_rpm_01_loop.ogg
+REM ffmpeg -y -i %SOURCE_AUDIO%\cc_sfx_vehicle01_rpm_01_loop.wav -filter:a %SILENT% -acodec libvorbis %CONTENT_AUDIO%\cc_sfx_vehicle01_rpm_01_loop.ogg
+REM ffmpeg -y -i %SOURCE_AUDIO%\cc_sfx_vehicle01_rpm_02_loop.wav -filter:a %SILENT% -acodec libvorbis %CONTENT_AUDIO%\cc_sfx_vehicle01_rpm_02_loop.ogg
+REM ffmpeg -y -i %SOURCE_AUDIO%\cc_sfx_vehicle02_rpm_01_loop.wav -filter:a %SILENT% -acodec libvorbis %CONTENT_AUDIO%\cc_sfx_vehicle02_rpm_01_loop.ogg
 ffmpeg -y -i %SOURCE_AUDIO%\cc_sfx_vehicle02_rpm_02_loop.wav -filter:a %SILENT% -acodec libvorbis %CONTENT_AUDIO%\cc_sfx_vehicle02_rpm_02_loop.ogg
 
 REM "ambient" sounds
-ffmpeg -y -i %SOURCE_AUDIO%\cc_sfx_ocean_loop.wav -filter:a %SILENT% -acodec libvorbis %CONTENT_AUDIO%\cc_sfx_ocean_loop.ogg
+ffmpeg -y -i %SOURCE_AUDIO%\cc_sfx_ocean_loop.wav -filter:a %MED% -acodec libvorbis %CONTENT_AUDIO%\cc_sfx_ocean_loop.ogg
 
 
 ffmpeg -y -i %SOURCE_AUDIO%\cc_sfx_turbine01_noise_loop.wav -filter:a %NORMAL% -acodec libvorbis %CONTENT_AUDIO%\cc_sfx_turbine01_noise_loop.ogg
@@ -22,8 +24,8 @@ ffmpeg -y -i %SOURCE_AUDIO%\cc_sfx_turbine01_rpm_loop.wav -filter:a %NORMAL% -ac
 
 ffmpeg -y -i %SOURCE_AUDIO%\cc_sfx_turbine02_noise_loop.wav -filter:a %NORMAL% -acodec libvorbis %CONTENT_AUDIO%\cc_sfx_turbine02_noise_loop.ogg
 
-ffmpeg -y -i %SOURCE_AUDIO%\wind_outdoor_high.wav -filter:a %SILENT% -acodec libvorbis %CONTENT_AUDIO%\wind_outdoor_high.ogg
-ffmpeg -y -i %SOURCE_AUDIO%\wind_outdoor_low.wav -filter:a %SILENT% -acodec libvorbis %CONTENT_AUDIO%\wind_outdoor_low.ogg
-ffmpeg -y -i %SOURCE_AUDIO%\wind_outdoor_medium.wav -filter:a %SILENT% -acodec libvorbis %CONTENT_AUDIO%\wind_outdoor_medium.ogg
+ffmpeg -y -i %SOURCE_AUDIO%\wind_outdoor_low.wav -filter:a %MEDLOW% -acodec libvorbis %CONTENT_AUDIO%\wind_outdoor_low.ogg
+ffmpeg -y -i %SOURCE_AUDIO%\wind_outdoor_medium.wav -filter:a %MEDLOW% -acodec libvorbis %CONTENT_AUDIO%\wind_outdoor_medium.ogg
+ffmpeg -y -i %SOURCE_AUDIO%\wind_outdoor_high.wav -filter:a %MEDLOW% -acodec libvorbis %CONTENT_AUDIO%\wind_outdoor_high.ogg
 
-copy nul mod.xml
+REM copy nul mod.xml
